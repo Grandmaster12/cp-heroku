57336ef1a2d0f8902da571fe8363d572ab90acb8
Sean Petersen <sean_p@uni.minerva.edu>
2023-02-10 18:43:39 -0800

    Removed port number, added user's name on signup

--- website/auth.py
+++ website/auth.py
@@ -63,7 +63,7 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             login_user(new_user, remember=True)
-            flash("Account successfully created!", category="success")
+            flash(f"Account successfully created! Welcome, {first_name}!", category="success")
             return redirect(url_for("views.index"))
 
     return render_template("sign_up.html", user=current_user)
