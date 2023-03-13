ff73ff76614ad0746f0c7cff9e337f2bd2e0000b
Sean Petersen <sean_p@uni.minerva.edu>
2023-03-12 20:54:22 -0700

    Removed dotenv from requirements dotenv was not working when deployed on Heroku but worked locally

--- CP_func.py
+++ CP_func.py
@@ -1,11 +1,11 @@
 # code file that imports the pre-written data, runs the randomiser, and makes the OpenAI calls
 import random, re, openai, os
-from dotenv import load_dotenv
 from boto.s3.connection import S3Connection
 
 try: 
     openai.api_key = S3Connection(os.environ['S3_KEY'])
 except:
+    from dotenv import load_dotenv
     load_dotenv()
     openai.api_key = os.getenv("API_KEY")
 
